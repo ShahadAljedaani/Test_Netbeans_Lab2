1e3beaf81f2f372a6638031b195bdf056f7f0738
ShahadAljedaani <Shal0001@stu.kau.edu.sa>
2024-02-09 16:09:39 +0300

    Hello I'm Shahad Aljedaani I wrote I print statment

---
+++ src/pkg251_test_lab2/Main.java
@@ -0,0 +1,14 @@
+
+package pkg251_test_lab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World !");
+        
+
+
+    }
+    
+}
